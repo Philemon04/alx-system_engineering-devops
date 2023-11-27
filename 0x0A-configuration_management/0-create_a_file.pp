e5d7763daf6382fe0d6d2644c03162b79e4864be
Phil <83282650+Philemon04@users.noreply.github.com>
2023-11-27 14:34:05 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-file.path# Create a file in /tmp.
+# Creates a file in /tmp
+
 file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
